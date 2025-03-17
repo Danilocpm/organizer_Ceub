4186be224a25d1d7e78fcee1245656909a189f75
Danilo <danilocarneirop@gmail.com>
2025-03-17 20:36:15 -0300

    update do requirements

--- app/models/turma.py
+++ app/models/turma.py
@@ -7,7 +7,7 @@ class Turma(models.Model):
     professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
     materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
     dia_da_semana = models.ForeignKey(DiasDaSemana, on_delete=models.CASCADE)
-    sala = models.CharField(max_length=20)
+    sala = models.CharField(max_length=20) 
 
     class Meta:
         unique_together = ('professor', 'materia', 'dia_da_semana')
