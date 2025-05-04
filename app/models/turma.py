f2e421067e36a9c56c58a8fb76a27b9cfb158c90
Danilocpm <danilocarneirop@gmail.com>
2025-05-03 22:38:23 -0300

    tirando obrigatoriedade do dia da semana na turma

--- app/models/turma.py
+++ app/models/turma.py
@@ -7,7 +7,7 @@ from .campus import Campus
 class Turma(models.Model):
     professor = models.ForeignKey(Professor, on_delete=models.CASCADE, null=True, blank=True)
     materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
-    dia_da_semana = models.ForeignKey(DiasDaSemana, on_delete=models.CASCADE)
+    dia_da_semana = models.ForeignKey(DiasDaSemana, on_delete=models.CASCADE, null=True, blank=True)
     campus = models.ForeignKey(Campus, on_delete=models.CASCADE, null=True, blank=True) # Added campus ForeignKey, allow null
     sala = models.CharField(max_length=20)
     tag = models.CharField(max_length=3, null=True, blank=True) # Added tag field, allow null
@@ -15,7 +15,7 @@ class Turma(models.Model):
 
     class Meta:
         # Updated unique_together to include campus
-        unique_together = ('professor', 'materia', 'dia_da_semana', 'campus')
+        unique_together = ('professor', 'materia', 'campus')
 
     def __str__(self):
         # Updated __str__ to include campus and status, removed professor as it's now nullable
