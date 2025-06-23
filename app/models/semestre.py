cd33db66295ef202c21132f633279204ce0bd905
Danilocpm <danilocarneirop@gmail.com>
2025-06-22 21:25:19 -0300

    atualizacao db

--- app/models/semestre.py
+++ app/models/semestre.py
@@ -1,9 +1,18 @@
 from django.db import models
 from .turma import Turma
+from .campus import Campus
 
 class Semestre(models.Model):
     id = models.AutoField(primary_key=True) # Explicitly define auto-incrementing primary key
-    semestre = models.IntegerField(unique=True) # Assuming semester number should be unique
+    semestre = models.IntegerField()
+    TURNO_CHOICES = [
+        ('MANHA', 'Manhã'),
+        ('TARDE', 'Tarde'),
+        ('NOITE', 'Noite'),
+    ]
+    turno = models.CharField(max_length=5, choices=TURNO_CHOICES, default='MANHA')
+    tag = models.CharField(max_length=3)
+    campus = models.ForeignKey(Campus, on_delete=models.CASCADE)
     turmas = models.ManyToManyField(Turma, blank=True) # Allows a semestre to exist without turmas initially
 
     def __str__(self):
